27f4230780b07d5618ca8b51fefb3f312126b994
Renuka Tiwari <cloudrohu@gmail.com>
2026-06-27 11:12:09 +0530

    mark_safe

--- utility/models.py
+++ utility/models.py
@@ -14,7 +14,7 @@ from django.utils.text import slugify
 
 class Postal_Code(models.Model):    
     postal_name = models.CharField(max_length=500,blank=True, null=True,)
-    postal_code = models.CharField(max_length=6,blank=True, null=True,)
+    postal_code = models.CharField(max_length=6,blank=True, null=True,unique=True)
 
     def __str__(self):
         return self.postal_name 
